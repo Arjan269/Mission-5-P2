9302645a248dd4ba89bd5d47d76d1301cbb8820e
Arjan269 <Arjanp@missionreadyhq.com>
2025-12-01 11:08:28 +1300

    final inital commit XD

--- client/z-energy/src/pages/Price-comp.jsx
+++
@@ -1,8 +0,0 @@
-export default function PriceComp() {
-  return (
-    <div>
-      <h1>Price Comparison Page</h1>
-      <p>This will be your pricing/comparison feature.</p>
-    </div>
-  );
-}
--- client/z-energy/src/pages/Z-maps.jsx
+++
@@ -1,8 +0,0 @@
-export default function ZMaps() {
-  return (
-    <div>
-      <h1>Maps Page</h1>
-      <p>This is your Maps page.</p>
-    </div>
-  );
-}
