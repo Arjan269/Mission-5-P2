febb90ff9b3487519939c1c589be2e219d003733
Rithvik Sharma <rsha264@aucklanduni.ac.nz>
2025-12-02 15:29:38 +1300

    ok now it works

--- client/z-energy/src/App.jsx
+++ client/z-energy/src/App.jsx
@@ -3,16 +3,17 @@ import Home from "./pages/Home/Home";
 import Locations from "./pages/Locations/locations";
 import PriceComp from "./pages/FuelPriceComparePage/PriceComp"; // NEW IMPORT
 import Navbar from "./common/Navbar";
+import styles from "./App.module.css";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="app-container">
-        <header className="header">
+      <div className={styles.appContainer}>
+        <header className={styles.header}>
           <Navbar />
         </header>
 
-        <main className="container">
+        <main className={styles.container}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/locations" element={<Locations />} />
--- client/z-energy/src/pages/Locations/locations.jsx
+++ client/z-energy/src/pages/Locations/locations.jsx
@@ -2,7 +2,7 @@ import styles from "./locations.module.css";
 export default function Location() {
   return (
     <div className={styles.container}>
-
+      <p>yuh</p>
     </div>
   );
 }
