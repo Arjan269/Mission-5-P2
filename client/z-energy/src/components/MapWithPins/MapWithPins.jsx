0978cc7e7c7d56a5cfcd800e3bf733554769811b
Rithvik Sharma <rsha264@aucklanduni.ac.nz>
2025-12-04 20:58:12 +1300

    resetting zoom when new station card is clicked

--- client/z-energy/src/components/MapWithPins/MapWithPins.jsx
+++ client/z-energy/src/components/MapWithPins/MapWithPins.jsx
@@ -1,4 +1,5 @@
 import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import { useRef, useEffect } from "react";
 import styles from "./MapWithPins.module.css";
 
 export default function MapWithPins({
@@ -10,22 +11,36 @@ export default function MapWithPins({
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
   });
 
-  const center = selectedStation
-    ? {
+  const mapRef = useRef(null);
+
+  const onLoad = (map) => {
+    mapRef.current = map;
+  };
+
+  useEffect(() => {
+    if (mapRef.current && selectedStation) {
+      mapRef.current.setZoom(14); 
+      mapRef.current.panTo({
         lat: selectedStation.coordinates.lat,
         lng: selectedStation.coordinates.lng,
-      }
-    : { lat: stations[0].coordinates.lat, lng: stations[0].coordinates.lng };
+      });
+    }
+  }, [selectedStation]);
+
+  const defaultCenter = {
+    lat: stations[0].coordinates.lat,
+    lng: stations[0].coordinates.lng,
+  };
 
   return (
     <div className={styles.outerDiv}>
       {isLoaded && (
         <GoogleMap
+          onLoad={onLoad}
           mapContainerStyle={{ width: "100%", height: "100%" }}
-          center={center}
+          center={defaultCenter} 
           zoom={14}
         >
-          {/* Show all pins */}
           {stations.map((station) => (
             <Marker
               key={station._id}
