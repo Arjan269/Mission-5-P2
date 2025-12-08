bc02635d5b23ef862e9e262a81086c307ad249ab
Rithvik Sharma <rsha264@aucklanduni.ac.nz>
2025-12-09 08:22:18 +1300

    changing ports

--- server/server.js
+++ server/server.js
@@ -25,5 +25,5 @@ app.get("/", (req, res) => {
 });
 
 // Server listen
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
